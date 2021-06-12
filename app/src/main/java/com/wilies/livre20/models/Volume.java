ce99f0ebf50c2880c9406fd4092f22f8be10e759
wanjala <gwiliez@gmail.com>
2021-06-12 22:19:57 +0300

    define models

---
+++ app/src/main/java/com/wilies/livre20/models/Volume.java
@@ -0,0 +1,31 @@
+package com.wilies.livre20.models;
+
+public class Volume {
+    private String kind;
+    private String id;
+    private VolumeInfo volumeInfo;
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public VolumeInfo getVolumeInfo() {
+        return volumeInfo;
+    }
+
+    public void setVolumeInfo(VolumeInfo volumeInfo) {
+        this.volumeInfo = volumeInfo;
+    }
+}
